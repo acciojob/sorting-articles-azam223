2087dff9be117f67d8bbb72ab5f950faa6f529f1
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-03 19:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled'];
+//your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-function stripArticle(title) {
-    // Remove "a", "an", and "the" from the beginning of the title
-    return title.replace(/^(a |an |the )/i, '').trim();
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an )/i, '').trim();
 }
 
-const sortedBands = bands.slice().sort((a, b) => stripArticle(a) > stripArticle(b) ? 1 : -1);
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
 const bandList = document.getElementById('band');
-
 sortedBands.forEach(band => {
-    const listItem = document.createElement('li');
-    listItem.textContent = band;
-    bandList.appendChild(listItem);
+    const li = document.createElement('li');
+    li.textContent = band;
+    bandList.appendChild(li);
 });
